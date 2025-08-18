b27d2d67944592063b197fd4b1d7756d6d6aa4a9
MatiasCarnero <mcarnero@mail.austral.edu.ar>
2025-08-18 12:38:46 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -12,29 +12,29 @@ suma = num1 + num2
 print("Suma:", suma)
 
 diferencia = num1 - num2
-print("Diferencia:", diferencia)
+print(diferencia)
 
 producto = num1 * num2
-print("Producto:", producto)
+print(producto)
 
 promedio = (num1 + num2) / 2
-print("Promedio:", promedio)
+print(promedio)
 
 if num2 != 0:
     cociente_entero = num1 // num2
-    print("Cociente entero:", cociente_entero)
+    print(cociente_entero)
 else:
     print("Cociente entero: No definido (división por cero)")
 
 if num2 != 0:
     resto = num1 % num2
-    print("Resto:", resto)
+    print(resto)
 else:
     print("Resto: No definido (división por cero)")
 
 if num2 != 0:
     division_real = num1 / num2
-    print("División real:", division_real)
+    print(division_real)
 else:
     print("División real: No definido (división por cero)")
 
